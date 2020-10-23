546d2089dae4b3ed0adc657a91b7cb1c6574c89e
sky759 <1063811038@qq.com>
2020-10-23 10:12:42 +0800

    c语言课程

---
+++ c语言课程/加和.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a=0;
+	int b=0;
+	int sum=0;
+	printf("请输入两个数：\n"); 
+	scanf("%d %d",&a,&b);
+	printf("得到的结果是：%d",sum=a+b);
+	return 0;
+} 
---
+++ c语言课程/未命名1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int age=0;
+	printf("Ëï¿µÒã\n");
+	scanf("%d",&age);
+	printf("Ëï¿µÒãµÄÄêÁäÊÇ%dËê\n",age);																					
+	return 0;
+}
