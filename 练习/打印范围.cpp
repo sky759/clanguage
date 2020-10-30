2d88d82197dc1256993edebea5672c4696359197
sky759 <1063811038@qq.com>
2020-10-30 11:36:39 +0800

    打印范围

--- 练习/打印范围.cpp
+++ 练习/打印范围.cpp
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main(){
 	int a,b,i;
-	printf("«Î ‰»Î¥Ú”°∑∂Œß£∫\n");
+	printf("«Î ‰»Î¥Ú”°∑∂Œß\n");
     scanf("%d %d",&a,&b);
 	for(i=a;i<=b;i++)
 	{printf("%d\n",i);}
 	return 0;
 } 
- 
